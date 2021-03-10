43e096f3528529f4178ff382a1fd85b66753d412
radwanaltaf <radwanaltaf2000@gmail.com>
2021-03-11 01:29:48 +0800

    Wiris Moved Inside Package

--- index.js
+++ index.js
@@ -84,12 +84,23 @@ const BeautyWebView = ({
 
   const html = `
     <html>
-        <head>
-            <script src="../../../../node_modules/@wiris/mathtype-generic/wirisplugin-generic.js"></script>
-        </head>
-        <body>
-          <h1>HELLOOO</h1>
-        </body>
+      <head>
+          <script src="./node_modules/@wiris/mathtype-generic/wirisplugin-generic.js"></script>
+      </head>
+      <body>
+          <div id="toolbar"></div>
+          <div id="htmlEditor" contenteditable="true">Try me!</div>
+        <script>
+          var genericIntegrationProperties = {};
+          genericIntegrationProperties.target = document.getElementById('htmlEditor');
+          genericIntegrationProperties.toolbar = document.getElementById('toolbar');
+    
+          // GenericIntegration instance.
+          var genericIntegrationInstance = new WirisPlugin.GenericIntegration(genericIntegrationProperties);
+          genericIntegrationInstance.init();
+          genericIntegrationInstance.listeners.fire('onTargetReady', {});
+        </script>
+      </body>
     </html>
     `
 
